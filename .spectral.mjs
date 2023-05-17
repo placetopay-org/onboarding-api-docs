fad4a475fd2a79372ad35324cb9fd8508d5d7965
alejandro.silva@evertecinc.com <alejandro.silva@evertecinc.com>
2023-05-17 15:20:23 -0500

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjM5Mzc/spectral.js?branch_slug=master&token=4126b10c-209a-4368-9bd1-6832bb1cbfab";
+export default { extends: ruleset };
